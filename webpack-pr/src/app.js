51c714e8c96aa789600c21ea681dec7c4ce073eb
vlad <eric@example.com>
2019-04-12 16:19:07 +0300

    ver 3

--- webpack-pr/src/app.js
+++ webpack-pr/src/app.js
@@ -2,7 +2,8 @@ import './js/rainbows.js';
 import './js/unicorns.js';
 import './scss/base.scss';
 //import Icon from './images/logo.png';
-
+//import '.images/voshod.png';
 //const img = new Image();
+//import LogoImg from '.images/voshod.png';
 //img.src = Icon;
 //element.appendChild(img);
--- webpack-pr/webpack.config.js
+++ webpack-pr/webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -17,12 +18,38 @@ module.exports = {
         })
       },
       {
-         test: /\.(png|jpg)$/,
-          loader: 'url-loader'
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('style.css')
+    new ExtractTextPlugin('style.css'),
+    new CopyWebpackPlugin([{
+        from: './src/fonts',
+        to: './fonts'
+      },
+      {
+        from: './src/favicon',
+        to: './favicon'
+      },
+      {
+        from: './src/images',
+        to: './images'
+      },
+      {
+        from: './src/uploads',
+        to: './uploads'
+      }
+    ]),
   ]
 };
